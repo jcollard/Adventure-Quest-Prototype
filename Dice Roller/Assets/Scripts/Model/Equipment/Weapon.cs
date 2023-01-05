f42488e3d947544ef46c798c38dc799d79e5029e
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-05 08:24:53 -0700

    refactor: Weapon to meet new IEquipable interface

--- Dice Roller/Assets/Scripts/Model/Equipment/Weapon.cs
+++ Dice Roller/Assets/Scripts/Model/Equipment/Weapon.cs
@@ -1,21 +1,30 @@
 
+using System.Collections.Generic;
+using AdventureQuest.Character;
 using AdventureQuest.Character.Dice;
+using System.Linq;
 
 namespace AdventureQuest.Equipment
 {
     public class Weapon : IEquipable
     {
-        public Weapon(string name, int cost, AbilityRoll damage)
+        private List<IRequirement> _requirements;
+
+        public Weapon(string name, int cost, AbilityRoll damage) : this(name, cost, damage, new List<IRequirement>()) { }
+
+        public Weapon(string name, int cost, AbilityRoll damage, List<IRequirement> requirements)
         {
             Name = name;
             Cost = cost;
             Damage = damage;
+            _requirements = requirements.ToList();
         }
 
         public string Name { get; }
         public int Cost { get; }
         public AbilityRoll Damage { get; }
-        public EquipmentType EquipmentType => EquipmentType.Weapon;
+        public HashSet<EquipmentSlot> Slots => new() { EquipmentSlot.LeftHand, EquipmentSlot.RightHand };
+        public List<IRequirement> Requirements => _requirements.ToList();
 
     }
 }
