48bfe800603145d56fc8ec17b349cb4f1a28b7a3
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-05 10:35:00 -0700

    refactor: Update IEquipmenttManifest interface Equipped method => Equipped property (Dictionary) Add IEquipmentManifestExtensions which adds an Equip helper method.

--- Dice Roller/Assets/Scripts/Model/Equipment/IEquipmentManifest.cs
+++ Dice Roller/Assets/Scripts/Model/Equipment/IEquipmentManifest.cs
@@ -4,9 +4,14 @@ namespace AdventureQuest.Equipment
 {
     public interface IEquipmentManifest
     {
+        public Dictionary<EquipmentSlot, IEquipable> Equipped { get; }
         public bool Equip(IEquipable toEquip, List<EquipmentSlot> slot);
         public bool Unequip(EquipmentSlot type);
         public bool IsEquipped(EquipmentSlot type);
-        public IEquipable Equipped(EquipmentSlot type);
+    }
+
+    public static class IEquipmentManifestExtensions
+    {
+        public static bool Equip(this IEquipmentManifest manifest, IEquipable toEquip, EquipmentSlot slot) => manifest.Equip(toEquip, new List<EquipmentSlot>() { slot });
     }
 }
