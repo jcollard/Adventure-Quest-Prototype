548dfc92ba9b19ea9e0d634111a0e91373c12c6e
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-05 10:32:11 -0700

    feat: Add Parse method to AbilityRoll.

--- Dice Roller/Assets/Scripts/Model/Character/Dice/AbilityRoll.cs
+++ Dice Roller/Assets/Scripts/Model/Character/Dice/AbilityRoll.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AdventureQuest.Dice;
 
 namespace AdventureQuest.Character.Dice
@@ -14,5 +15,28 @@ namespace AdventureQuest.Character.Dice
         public RollModifier Modifier { get; }
 
         public int Roll(Abilities abilities) => BaseRoll.Roll() + Modifier.ModifyWith(abilities);
+
+        public static AbilityRoll Parse(string diceNotation)
+        {
+            string[] tokens = diceNotation.Trim().Split("+");
+            List<DiceGroup> diceGroup = new ();
+            List<string> rollModiferTokens = new ();
+            foreach (string token in tokens)
+            {
+                if (DiceGroup.IsParseable(token))
+                {
+                    diceGroup.Add(DiceGroup.Parse(token));
+                }
+                else
+                {
+                    rollModiferTokens.Add(token);
+                }
+            }
+            string rollModiferNotation = string.Join("+", rollModiferTokens);
+            if (!RollModifier.IsParseable(rollModiferNotation)) { throw new System.FormatException($"Could not parse AbilityRoll from \"{diceNotation}\"."); }
+            DicePool dicePool = new (diceGroup);
+            RollModifier rollModifier = RollModifier.Parse(rollModiferNotation);
+            return new AbilityRoll(dicePool, rollModifier);
+        }
     }
 }
