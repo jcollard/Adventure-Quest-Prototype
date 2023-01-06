3b448fb8f3d34b37678835fa3ff52824b187a219
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-06 14:31:21 -0700

    feat: Rename PlayerCharacterController

---
+++ Dice Roller/Assets/Scripts/Controller/Character/CharacterController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace AdventureQuest.Character
+{
+    [RequireComponent(typeof(ObservableCharacter))]
+    public class CharacterController : MonoBehaviour
+    {
+        private ICharacter _playerCharacter;
+
+        [field: SerializeField]
+        public bool LoadFromStorageOnLoad { get; private set; }
+        private ObservableCharacter _observable;
+
+        protected void Awake()
+        {
+            _observable = GetComponent<ObservableCharacter>();
+            _observable.OnChange.AddListener((character) => _playerCharacter = character);
+        }
+
+        protected void Start()
+        {
+            if (LoadFromStorageOnLoad)
+            {
+                _observable.Observed = PlayerCharacter.Restore();
+                return;
+            }
+        }
+
+    }
+}
