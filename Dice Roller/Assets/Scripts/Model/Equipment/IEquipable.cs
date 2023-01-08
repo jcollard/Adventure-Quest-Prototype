dea2ade786032dfd83339bc2f254cb4a1b29497b
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-08 08:41:45 -0700

    feat: Add static FromJson to IEquipable.

--- Dice Roller/Assets/Scripts/Model/Equipment/IEquipable.cs
+++ Dice Roller/Assets/Scripts/Model/Equipment/IEquipable.cs
@@ -8,5 +8,20 @@ namespace AdventureQuest.Equipment
     {
         public HashSet<EquipmentSlot> Slots { get; }
         public List<IRequirement> Requirements { get; }
+
+        public static IEquipable FromJson(string json)
+        {
+            // TODO: Better serialization is required for extensibility
+            try
+            {
+                return UnityEngine.JsonUtility.FromJson<Weapon>(json);
+            }
+            catch
+            {
+
+            }
+            throw new System.Exception("Could not load IEquipable");
+        }
+
     }
 }
