1cd1814e7764d3f4868707961b3481aa38dd439b
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-08 08:40:16 -0700

    feat: Implement serialization for AbilityRoll.

--- Dice Roller/Assets/Scripts/Model/Character/Dice/AbilityRoll.cs
+++ Dice Roller/Assets/Scripts/Model/Character/Dice/AbilityRoll.cs
@@ -3,16 +3,20 @@ using AdventureQuest.Dice;
 
 namespace AdventureQuest.Character.Dice
 {
-    public class AbilityRoll
+    [System.Serializable]
+    public class AbilityRoll : UnityEngine.ISerializationCallbackReceiver
     {
+        [field: UnityEngine.SerializeField]
+        private string _diceNotation;
         public AbilityRoll(IRollable baseRoll, RollModifier modifier)
         {
             BaseRoll = baseRoll;
             Modifier = modifier;
         }
 
-        public IRollable BaseRoll { get; }
-        public RollModifier Modifier { get; }
+        public IRollable BaseRoll { get; private set; }
+        public RollModifier Modifier { get; private set; }
+        public string DiceNotation { get => ToString(); }
 
         public int Roll(Abilities abilities) => BaseRoll.Roll() + Modifier.ModifyWith(abilities);
 
@@ -37,6 +41,21 @@ namespace AdventureQuest.Character.Dice
             DicePool dicePool = new (diceGroup);
             RollModifier rollModifier = RollModifier.Parse(rollModiferNotation);
             return new AbilityRoll(dicePool, rollModifier);
+        }
+
+        public override string ToString() => $"{BaseRoll} + {Modifier}";
+
+        public void OnBeforeSerialize()
+        {
+            _diceNotation = this.ToString();
+        }
+
+        public void OnAfterDeserialize()
+        {
+            if (_diceNotation == null) { return; }
+            AbilityRoll parsed = AbilityRoll.Parse(_diceNotation);
+            BaseRoll = parsed.BaseRoll;
+            Modifier = parsed.Modifier;
         }
     }
 }
