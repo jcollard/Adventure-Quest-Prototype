4990bf061738183ed548200d9f9b0723dd73597d
Joseph Collard <jcollard@users.noreply.github.com>
2023-01-11 05:39:37 -0700

    feat: Implement Serialization for IRequirement

--- Dice Roller/Assets/Scripts/Model/Equipment/Requirement/IRequirement.cs
+++ Dice Roller/Assets/Scripts/Model/Equipment/Requirement/IRequirement.cs
@@ -1,9 +1,15 @@
 using AdventureQuest.Character;
+using AdventureQuest.Json;
 
 namespace AdventureQuest.Equipment.Requirement
 {
-    public interface IRequirement
+    public interface IRequirement : IJsonable
     {
         public bool MeetsRequirement(ICharacter character);
+
+        public static IRequirement Deserialize(string json)
+        {
+            return UnityEngine.JsonUtility.FromJson<WeaponRequirement>(json);
+        }
     }
 }
--- Dice Roller/Assets/Scripts/Model/Equipment/Requirement/WeaponRequirement.cs
+++ Dice Roller/Assets/Scripts/Model/Equipment/Requirement/WeaponRequirement.cs
@@ -2,8 +2,11 @@ using AdventureQuest.Character;
 
 namespace AdventureQuest.Equipment.Requirement
 {
+    [System.Serializable]
     public class WeaponRequirement : IRequirement
     {
+        public string AsJson => UnityEngine.JsonUtility.ToJson(this);
+
         public bool MeetsRequirement(ICharacter character)
         {
             foreach(IEquipable equipped in character.Equipment.Equipped.Values)
